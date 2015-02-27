f810cd37378224bed56b5ca5d3b9cc36b1eae534
Nishant Trivedi <ntrivedi@140-sfoengwifi55-123.clients.corp.yelpcorp.com>
2015-02-26 16:39:18 -0800

    Added code to track direction

--- processing.py
+++ processing.py
@@ -2,42 +2,65 @@ import cv2
 import numpy as np
 
 class ImageProcessor:
+    count = 0
 
-	def getContours(self, img):
-	    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-	    blur = cv2.GaussianBlur(gray,(5,5),0)
-	    ret, thresh1 = cv2.threshold(blur,70,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+    #fgbg = cv2.BackgroundSubtractorMOG()
 
-	    contours, hierarchy = cv2.findContours(thresh1,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    def getContours(self, img):
+        gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        blur = cv2.GaussianBlur(gray,(5,5),0)
 
-	    max_area = 0
-	    ci = 0
-	    for i in range(len(contours)):
-	            cnt = contours[i]
-	            area = cv2.contourArea(cnt)
-	            if(area > max_area):
-	                max_area = area
-	                ci = i
+        #masked_img = self.fgbg.apply(blur)
+        ret, thresh1 = cv2.threshold(blur,70,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
 
-	    cnt = contours[ci]
-	    hull = cv2.convexHull(cnt)
-	    """
-	    moments = cv2.moments(cnt)
-	    if moments['m00']!=0:
-	                cx = int(moments['m10']/moments['m00']) # cx = M10/M00
-	                cy = int(moments['m01']/moments['m00']) # cy = M01/M00
-	    centr=(cx,cy)       
-	    cv2.circle(img,centr,5,[0,0,255],2)       
-	    """
+        contours, hierarchy = cv2.findContours(thresh1,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
-	    #cv2.drawContours(drawing,[cnt],0,(0,255,0),2)
+        max_area = 0
+        ci = 0
+        for i in range(len(contours)):
+                cnt = contours[i]
+                area = cv2.contourArea(cnt)
+                if(area > max_area):
+                    max_area = area
+                    ci = i
 
-	    #cnt = cv2.approxPolyDP(cnt,0.01*cv2.arcLength(cnt,True),True)
-	    hand = np.array(sorted(cnt, key=lambda x:x[0][0], reverse=True)[0:len(cnt)*15/100])
+        cnt = contours[ci]
+        hull = cv2.convexHull(cnt, returnPoints = False)
+        defects = cv2.convexityDefects(cnt, hull)
 
-	    return cnt, hand
+        if defects is not None and len(defects) > 0:
+            sorted_defects = sorted(defects, key=lambda x:x[0][3], reverse=True)
+            farthest_point = cnt[sorted_defects[0][0][2]]
+        else:
+            farthest_point = [[0, 0]]
+
+        """
+        moments = cv2.moments(cnt)
+        if moments['m00']!=0:
+                    cx = int(moments['m10']/moments['m00']) # cx = M10/M00
+                    cy = int(moments['m01']/moments['m00']) # cy = M01/M00
+        centr=(cx,cy)       
+        cv2.circle(img,centr,5,[0,0,255],2)       
+        """
+
+        (x, y), rad = cv2.minEnclosingCircle(cnt)
+
+        if self.count >= 0:
+            if farthest_point[0][0] < x:
+                rev = True
+            else:
+                rev = False
+
+            self.count += 1
+
+        #cv2.drawContours(drawing,[cnt],0,(0,255,0),2)
+
+        #cnt = cv2.approxPolyDP(cnt,0.01*cv2.arcLength(cnt,True),True)
+        hand = np.array(sorted(cnt, key=lambda x:x[0][0], reverse=rev)[0:len(cnt)*15/100])
+
+        return cnt, hand
 
 
-	    #cv2.drawContours(drawing,[hull],0,(0,0,255),2) 
-	    
-	    #hull = cv2.convexHull(cnt,returnPoints = False)
+        #cv2.drawContours(drawing,[hull],0,(0,0,255),2) 
+        
+        #hull = cv2.convexHull(cnt,returnPoints = False)
